f9d78a48067cafe0141b4405811e8b5030bf8598
ayush1109 <46477990+ayush1109@users.noreply.github.com>
2019-01-14 23:58:39 +0530

    new files added

--- program3.java
+++ program3.java
@@ -1,7 +1,14 @@
 import java.util.*;
 class program3{
 	public static void main(String args[]){
-		System.out.println("Integer Type" + "\t" + Integer.size + "\t" + Integer.mn_value + "\t\t" + Integer.max_value);
+		Scanner s= new Scanner(System.in);
+		int a;
+		int b;
+		boolean b1;
+		boolean b2;
+		System.out.print("Enter two integer values:");
+		a = s.nextInt();
 		
+
 	}
 }
