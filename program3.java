0e6220c1761154e966632d4a89485c01f337e8e6
ayush1109 <46477990+ayush1109@users.noreply.github.com>
2019-01-14 23:56:50 +0530

    new files added

---
+++ program3.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class program3{
+	public static void main(String args[]){
+		System.out.println("Integer Type" + "\t" + Integer.size + "\t" + Integer.mn_value + "\t\t" + Integer.max_value);
+		
+	}
+}
